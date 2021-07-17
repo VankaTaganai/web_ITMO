06b5c6a082fa99c88a5da92669d87919f6a56f7d
VankaTaganai <panovivan01@yandex.ru>
2021-07-17 11:16:27 +0300

    hw11 added

---
+++ hw11/lesson11/frontend/src/components/middle/Index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <Post v-for="post in posts"
+              :post="post"
+              :login="getLoginByUserId(post.user.id)"
+              :key="post.id"
+        />
+    </div>
+</template>
+
+<script>
+import Post from "@/components/middle/Post"
+
+export default {
+    name: "Index",
+    components: {
+        Post,
+    },
+    props: ["posts", "users"],
+    methods: {
+        getLoginByUserId: function (userId) {
+            return this.users.find(user => user.id === userId).login
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
